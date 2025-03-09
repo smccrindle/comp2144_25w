2602b508284766e5a31a62ff678dbc321ee73199
Scott McCrindle <iainmccrindle@gmail.com>
2025-03-09 15:30:31 -0400

    Changed referenceSpaceType to local

--- lesson-7/js/ar-example.js
+++ lesson-7/js/ar-example.js
@@ -58,7 +58,7 @@ const createScene = async function() {
     const xr = await scene.createDefaultXRExperienceAsync({
         uiOptions: {
             sessionMode: "immersive-ar",
-            referenceSpaceType: "local-floor"
+            referenceSpaceType: "local"
         },
         // STEP 2b: Enable optional features - either all of them with true (boolean), or as an array
         optionalFeatures: true
